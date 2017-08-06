5762f512d2248952dc0e2d66f89df41b939fb563
Administrator <Administrator@yuanqiji>
2017-08-06 16:32:24 +0800

    。。

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -5,7 +5,7 @@ define([ 'jquery', 'underscore', 'backbone', 'router'], function($, _,
         Backbone, Router) {
     var initialize = function() {
         Router.initialize();
-    }
+    } 
     return {
         initialize : initialize
     };
